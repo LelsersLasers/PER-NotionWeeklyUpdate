08cd86ff10f1567072caf2c79ade04dd947b6653
LelsersLasers <millankumar@gmail.com>
2026-09-01 15:11:29 -0400

    nix flake for notion client?

---
+++ nix_flake/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Python env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      python = pkgs.python312;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          (python.withPackages (ps: with ps; [
+            notion-client
+          ]))
+        ];
+      };
+    };
+}
+
